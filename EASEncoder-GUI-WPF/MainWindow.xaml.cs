a664ad729d7ef308922429c6524346608b7d7175
Joseph Mignone <jmig3066@gmail.com>
2021-02-25 15:59:24 -0500

    Started designing WPF form

--- EASEncoder-GUI-WPF/MainWindow.xaml.cs
+++ EASEncoder-GUI-WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EASEncoder.Models.SAME;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.ComponentModel;
+using System.IO;
+using System.Reflection;
+using EASEncoder.Models;
+using NAudio.Wave;
 
 namespace EASEncoder_GUI_WPF
 {
@@ -20,6 +26,17 @@ namespace EASEncoder_GUI_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly List<SAMERegion> Regions = new List<SAMERegion>();
+        private string _length;
+        private SAMEMessageAlertCode _selectedAlertCode;
+        private SAMECounty _selectedCounty;
+        private SAMEMessageOriginator _selectedOriginator;
+        private SAMEState _selectedState;
+        //private SAMESubdivision _selectedSubdivision;
+        private string _senderId;
+        private DateTime _start;
+        private WaveOutEvent player;
+
         public MainWindow()
         {
             InitializeComponent();
